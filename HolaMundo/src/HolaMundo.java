256367133d55f5264b2365aa56ca05e66ebf6805
Abi Rivas <abi.rivas193@std.uni.edu.ni>
2025-06-21 14:56:03 -0600

    cambio de saludo

--- HolaMundo/src/HolaMundo.java
+++ HolaMundo/src/HolaMundo.java
@@ -2,6 +2,10 @@ public class HolaMundo {
     public static void main(String[] args) throws Exception {
         
         
+        System.out.println("Mi nombre es Abi Joyce Rivas Herrera");
+        System.out.println(" Mi numero de carnet es: 2025-1376U");
+
+
         String nombre="Estudiante";
         
         int a = 10;
